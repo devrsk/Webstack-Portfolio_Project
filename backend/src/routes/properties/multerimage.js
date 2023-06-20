9c42e524108a9bdb15a2fde169e97150a30a68d2
Raheem Salifu Kasim <r.kasim@aol.com>
2023-06-20 20:20:18 +0000

    Task Update

--- backend/src/routes/properties/multerimage.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require('express');
-const router = Router();
-
-const uploadImage = require('../../controllers/multerImage');
-
-//upload the property image in the post to the server
-router.route("/").post(uploadImage);
-
-module.exports = router;
